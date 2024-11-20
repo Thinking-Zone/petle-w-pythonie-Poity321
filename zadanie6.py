999c18734fbb5c54d2ae6f23f3f7354ff1f9da58
Poity321 <gautograf@gmail.com>
2024-11-20 18:37:33 +0100

    Update zadanie6.py

---
+++ zadanie6.py
@@ -0,0 +1,6 @@
+# Przepis na książkę edukacyjną: 
+# 1. Wybierz temat edukacyjny (np. matematyka, geografia, historia). 
+# 2. Podziel książkę na poziomy trudności. 
+# 3. Dodaj interaktywne elementy, np. pytania, zagadki, łamigłówki.
+# 4. Po każdej odpowiedzi daj szczegółowe wyjaśnienie.
+# 5. Stwórz system punktacji i nagród za postępy.
